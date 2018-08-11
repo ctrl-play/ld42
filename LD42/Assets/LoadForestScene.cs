11aeeded660598e6f4602c7398231b9a427b930e
Redleg281 <jacob@redlegranch.com>
2018-08-10 19:53:13 -0700

    Worked on map

---
+++ LD42/Assets/LoadForestScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadForestScene : MonoBehaviour {
+
+
+
+
+
+
+    public int num;
+
+// Use this for initialization
+void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(num);
+            Debug.Log("Scene changed to " + num);
+        }
+    }
+}
