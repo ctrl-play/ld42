a1556f88e91a05664d8c80476bcb160c4287685a
Redleg281 <jacob@redlegranch.com>
2018-08-11 13:51:00 -0700

    Updated Map

---
+++ dist/Assets/Scripts/LoadSceneSprite.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneSprite : MonoBehaviour {
+
+
+
+    // SceneManager.LoadScene(3);
+    // Debug.Log("it worked");
+
+
+    // Use this for initialization
+    //void Start () {
+
+    //}
+
+
+
+    void CastRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            
+        }
+
+        
+
+    }
+
+    // Update is called once per frame
+
+    void Update () {
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed left click, casting ray.");
+            CastRay();
+            Debug.Log("Hit object: " + collider.gameObject.name);
+        }
+
+
+    }
+}
