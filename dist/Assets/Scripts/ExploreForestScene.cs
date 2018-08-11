d007599cf1fde03d9240d65bacbc005eb10d6569
Redleg281 <jacob@redlegranch.com>
2018-08-11 10:43:02 -0700

    Add Buttons

--- dist/Assets/Scripts/ExploreForestScene.cs
+++ dist/Assets/Scripts/ExploreForestScene.cs
@@ -2,26 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class LoadForestScene : MonoBehaviour {
 
-
-
-
+public class ExploreForestScene : MonoBehaviour {
 
 
     public int num;
 
-// Use this for initialization
-void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
+
         if (Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene(num);
             Debug.Log("Scene changed to " + num);
         }
+
+
+
     }
 }
