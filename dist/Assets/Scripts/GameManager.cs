e5e8077db1b20d722103bf5ca32a5b32c68ffa71
Arjun M <bytemillennium@gmail.com>
2018-08-11 11:42:00 -0500

    Modified map
    Created explore scene and changed the map from UI to sprites

--- dist/Assets/Scripts/GameManager.cs
+++ dist/Assets/Scripts/GameManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     public static GameManager Instance { get; private set; }
+
+    int characterType;
+    string loadType;
+
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
